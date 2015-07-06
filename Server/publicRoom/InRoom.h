27f1e81a553d51475980c59f2409c1f4b841abdd
coderguang <royalchen@royalchen.com>
2015-07-06 20:09:56 +0800

    change to select module

--- Server/publicRoom/InRoom.h
+++ Server/publicRoom/InRoom.h
@@ -13,9 +13,6 @@
 #include <fcntl.h>
 #include <iostream>
 #include "../proto/Proto.h"
-#include "../include/Rdwr.h"
-#include "../include/InitFirst.h"
-#include "../include/Func.h"
 #include "../publicRoom/UpdateParty.h"
 #include "../proto/ProtoID.h"
 #include "../struct/PersonData.h"
@@ -23,44 +20,11 @@
 #include "../game/Gameing.h"
 using namespace std;
 
-//when the game in room ,belown loop solve the proto
-//
-//only receive chat ,party_change,game_start proto
 
 void InRoomLoop(int connfd){
 	
 	cout<<"lanuch success,come to publicRoom"<<endl;
 	
-	while(true){
-
-			sem_wait(statusmutex);
-			if(IN_GAME==statusptr->status){
-				sem_post(statusmutex);
-				InGameLoop(connfd);
-				break;
-			}
-			sem_post(statusmutex);
-				
-			int id=0;
-			int nread=read(connfd,&id,4);
-			
-		//cout<<"the id is "<<id<<endl;
-
-			if(nread<0){
-				if(errno!=EINTR){
-						cout<<"socket disconnections in the public room...."<<endl;
-						//if in the room disconnections.update the party
-						//shouldn't do this in here ,because it doesn't call sig_exit,the flag and conn is valid,but the socket disconnects,it makes write stream error!
-						//updateParty();
-						//DelayTime(5);
-						close(connfd);
-						exit(1);
-				}else{
-					 continue;			
-				}
-			}else if(0==nread)	/*EOF of the stream */
-					 continue;
-			
 			if(pChatID==id){
 					//cout<<"receive the chat in the room"<<endl;
 
@@ -93,15 +57,6 @@ void InRoomLoop(int connfd){
 					
 					cout<<"get the party change proto"<<endl;
 
-
-					//to avoid the death lock,the mutex order is listmutex-->nummutex;
-					//get the listmutex first to avoid the death lock
-
-					sem_wait(listmutex);
-
-					//if perhaps make the death lock,if someone exit in here
-					sem_wait(nummutex);//get the aother party's counter,if it is full,reject,otherwise,change the
-
 					cout<<"come to shm"<<endl;
 					
 					if(BLUE==PersonData::m_Party){
@@ -114,20 +69,11 @@ void InRoomLoop(int connfd){
 
 									Err_toc *temp=new Err_toc(PARTY_IS_FULL);
 									writen(connfd,&temp->id,sizeof(Err_toc));
-		
-									//post the mutex,the order is nummutex-->listmutex
-									sem_post(nummutex);
-									sem_post(listmutex);		
 	
 							}else{//change the party
 
 									numptr->blueCounter--;
 									numptr->redCounter++;
-
-									//reset the list
-									//sem_wait(listmutex); //the mutex order is listmutex-->nummutex to avoid the death lock
-
-									//cout<<"come to list shm in blue"<<endl;
 
 									for(int i=0;i<MAX_USER;i++){
 										if(1==listptr->flag[i]&&PersonData::m_ID==listptr->id[i]){
@@ -139,11 +85,7 @@ void InRoomLoop(int connfd){
 
 									}
 
-									sem_post(nummutex);
-									sem_post(listmutex);
-									
 									updateParty();//this need the listmutex,so must post before this
-
 
 									//talk to the client ,he's party is change
 									Party_change_tocs *t=new Party_change_tocs();
@@ -154,18 +96,9 @@ void InRoomLoop(int connfd){
 							if(numptr->blueCounter>=(MAX_USER/2)){//if another party if full
 									Err_toc *temp=new Err_toc(PARTY_IS_FULL);
 									writen(connfd,&temp->id,sizeof(Err_toc));
-									
-									sem_post(nummutex);
-									sem_post(listmutex);
-
-
 							}else{//change the party
 									numptr->blueCounter++;
 									numptr->redCounter--;
-
-									//reset the list
-								//	sem_wait(listmutex);
-									//cout<<"come to list shm in red"<<endl;
 
 									for(int i=0;i<MAX_USER;i++){
 										if(1==listptr->flag[i]&&PersonData::m_ID==listptr->id[i]){
@@ -176,10 +109,6 @@ void InRoomLoop(int connfd){
 										}					
 
 									}
-
-									sem_post(nummutex);
-									sem_post(listmutex);
-
 									updateParty();
 
 									//talk to the client ,he's party is change
@@ -190,8 +119,6 @@ void InRoomLoop(int connfd){
 				}else{//if get something wrong and didn't cast any befor,post the mutex
 
 							cout<<"didn't match any party!"<<endl;
-							sem_post(nummutex);
-							sem_post(listmutex);
 				}
 
 			}else if(gameStartID==id){//receive the start game proto
@@ -200,26 +127,6 @@ void InRoomLoop(int connfd){
 				GameStart_tocs *temp=new GameStart_tocs();
 
 				readn(connfd,&temp->error_code,sizeof(GameStart_tocs)-4);
-
-				//if(1000==PersonData::m_ID){//check if this proto from sg
-
-					//cout<<"receive the game start from sg"<<endl;
-					//change ths game status
-						
-					
-					sem_wait(statusmutex);
-					//cout<<"get the status mutex"<<endl;
-					statusptr->status=IN_GAME;
-					//cout<<"set the status mutex"<<endl;
-					sem_post(statusmutex);
-
-					//broadcast the GameStart_tocs to every client
-					//
-					//
-					//Chat_tocs *t=new Chat_tocs(1000,ALL,"game start after 10 seconds");
-				
-					//mBroadcast(ALL,t,sizeof(Chat_tocs));
-
 					
 					for(int i=0;i<5;i++){
 						string str="game start after seconds ";
@@ -233,21 +140,12 @@ void InRoomLoop(int connfd){
 
 					mBroadcast(ALL,temp,sizeof(GameStart_tocs));
 
-					sem_wait(statusmutex);
-					statusptr->status=IN_GAME;
-					sem_post(statusmutex);
-					
-				//}
-
 			}else{//some error sream ,receive it and do nothing
 					char buf[128];
 					readn(connfd,&buf,128);
-
-
 			}
 				
 
-	}
 
 }
 
--- Server/publicRoom/UpdateParty.h
+++ Server/publicRoom/UpdateParty.h
@@ -3,39 +3,30 @@
 #include "../struct/ShmServer.h"
 #include "../proto/Party_toc.h"
 #include "../include/BroadcastInterface.h"
-#include "../include/InitFirst.h"
 
 
-extern struct shmList *listptr;
-extern struct shmNum *numptr;
-
-extern sem_t *listmutex;
-extern sem_t *nummutex;
-
+extern struct shmList client[MAX_USER];
+extern struct shmNum numptr;
 
 void updateParty(){
+
 	int iblue=0; //the blue[] index
 	int ired=0; //the red[] index
 	
 	Party_toc *temp=new Party_toc();
 	
-	sem_wait(listmutex);
-
 	for(int i=0;i<MAX_USER;i++){
-		if(1==listptr->flag[i]){
-				if(BLUE==listptr->party[i]){
-					temp->blue[iblue++]=listptr->id[i]; //get the id to the party blue[] 
+		if(client[i].conn>0){
+				if(BLUE==client[i].party){
+					temp->blue[iblue++]=client[i].id; //get the id to the party blue[] 
 					continue;
-				}else if(RED==listptr->party[i]){
-					temp->red[ired++]=listptr->id[i];
+				}else if(RED==client[i].party){
+					temp->red[ired++]=client[i].id;
 					continue;
 				}					
 		}
 
 	}
-	sem_post(listmutex);
-	
-	//full the other []
 	
 	while(iblue<(MAX_USER/2)){
 			temp->blue[iblue++]=0;
@@ -44,7 +35,6 @@ void updateParty(){
 	while(ired<(MAX_USER/2)){
 			temp->red[ired++]=0;
 	}
-	
 	//broadcast this to the all
 	//
 	mBroadcast(ALL,temp,sizeof(Party_toc));
