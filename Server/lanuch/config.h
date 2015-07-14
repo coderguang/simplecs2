780046d2a594e119854786f96094faa2fb4fe04b
coderguang <royalchen@royalchen.com>
2015-07-14 10:57:00 +0800

    solve the exit bug

---
+++ Server/lanuch/config.h
@@ -0,0 +1,13 @@
+#ifndef NET_CONSTANT_
+#define NET_CONSTANT_
+
+
+const int PORT=9201;//server open  port
+const int MSG_SIZE=2048;//max msg
+const int BACKLOG=10;
+
+const int MAX_USER=128;
+
+
+
+#endif
